439e07b476b9074714c2d795b13fb1ba919738f1
Eldar <ealtymyshov2@gmail.com>
2023-04-09 23:20:28 +0600

    Переделал авторизацию

--- src/main/java/kg/iaau/amanalert/service/AuthService.java
+++ src/main/java/kg/iaau/amanalert/service/AuthService.java
@@ -2,7 +2,8 @@ package kg.iaau.amanalert.service;
 
 import kg.iaau.amanalert.model.auth.AuthModel;
 import kg.iaau.amanalert.model.auth.LoginRequestModel;
+import org.apache.tomcat.websocket.AuthenticationException;
 
 public interface AuthService {
-    AuthModel authorize(LoginRequestModel requestModel);
+    AuthModel authorize(LoginRequestModel requestModel) throws AuthenticationException;
 }
--- src/main/java/kg/iaau/amanalert/service/impl/AuthServiceImpl.java
+++ src/main/java/kg/iaau/amanalert/service/impl/AuthServiceImpl.java
@@ -4,16 +4,20 @@ import kg.iaau.amanalert.enums.Role;
 import kg.iaau.amanalert.model.auth.AuthModel;
 import kg.iaau.amanalert.model.auth.LoginRequestModel;
 import kg.iaau.amanalert.security.details.UserDetailsImpl;
+import kg.iaau.amanalert.security.details.UserDetailsServiceImpl;
 import kg.iaau.amanalert.service.AuthService;
 import kg.iaau.amanalert.util.JwtUtils;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,16 +27,18 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     AuthenticationManager authenticationManager;
     JwtUtils jwtUtils;
+    PasswordEncoder passwordEncoder;
+    UserDetailsService userDetailsService;
 
     @Override
-    public AuthModel authorize(LoginRequestModel requestModel) {
+    public AuthModel authorize(LoginRequestModel requestModel) throws AuthenticationException {
         log.info("username: {}", requestModel.getUsername());
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(requestModel.getUsername(), requestModel.getPassword()));
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService.loadUserByUsername(requestModel.getUsername());
 
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        if (!passwordEncoder.matches(requestModel.getPassword(), userDetails.getPassword())) {
+             throw new AuthenticationException("Не найден пользователь!");
+        }
 
         String jwt = jwtUtils.generateTokenFromUsername(userDetails);
 
